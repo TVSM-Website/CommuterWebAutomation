7d37d1cc86bca1ecceb9e870363cd4066517ba3a
ChetanWaliTvs <chethan.wali@tvsmotor.com>
2024-07-26 20:16:01 +0530

    Update Utilities.java

--- src/main/java/Utils/Utilities.java
+++ src/main/java/Utils/Utilities.java
@@ -28,7 +28,6 @@ public class Utilities
     {
         String propertiesFilePath;
 
-        // Determine the correct properties file based on the environment
         if (env.equalsIgnoreCase("UAT")) {
             propertiesFilePath = "src/test/Resources/BrandPageUrlUAT.properties";
         } else if (env.equalsIgnoreCase("PROD")) {
@@ -52,6 +51,98 @@ public class Utilities
         }
 
         return selectedVehicleUrl;
+    }
+    public static String getUrl(String environment) {
+        if ("UAT".equalsIgnoreCase(environment)) {
+            return "https://uat-www.tvsmotor.net/";
+        } else if ("PROD".equalsIgnoreCase(environment)) {
+            return "https://www.tvsmotor.com/";
+        } else {
+            throw new IllegalArgumentException("Invalid environment: " + environment);
+        }
+    }
+
+    public static void verifyUrl(WebDriver driver, String environment) {
+        String currentUrl = driver.getCurrentUrl();
+        if ("UAT".equalsIgnoreCase(environment)) {
+            if (!currentUrl.contains("uat")) {
+                throw new AssertionError("URL does not contain 'uat'");
+            }
+        } else if ("PROD".equalsIgnoreCase(environment)) {
+            if (currentUrl.contains("uat")) {
+                throw new AssertionError("URL contains 'uat'");
+            }
+        }
+    }
+
+    public static void clickElementUsingJS(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public static void scrollToView(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+    //Used in @Scooter to change '+' to 'plus' and lower case conversion
+    public static String normalizeString(String input) {
+        // Normalize the string by converting to lower case and replacing '+' with 'plus'
+
+        String normalized =  Normalizer.normalize(input, Normalizer.Form.NFD)
+                .replaceAll("\\p{M}", "")
+                .toLowerCase(Locale.ENGLISH)
+                .replace("+", " plus")
+                .replaceAll("[^a-z0-9]", " ")  // Replace non-alphanumeric characters with a space
+                .replaceAll("\\s+", " ")        // Remove all spaces
+                .trim();                       // Trim leading/trailing spaces
+        // Perform case-insensitive replacement of "orissa" with "odisha"
+        Pattern pattern = Pattern.compile("\\b(orissa)\\b", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(normalized);
+        normalized = matcher.replaceAll("odisha");
+        System.out.println(normalized + "here at utilities");
+        normalized.replaceAll("\\s+", "");
+        return normalized;
+    }
+
+    public static String normalizeStateName(String stateName) {
+        // Normalize the state name by converting to lower case, replacing variations of "orissa" with "odisha" for TVS Sport
+        String normalizedStateName = normalizeString(stateName);
+        return normalizedStateName;
+    }
+    public static WebElement refreshElement(WebElement element, WebDriver driver) {
+        // Get the locator from the WebElement
+        By locator = getLocatorFromElement(element);
+
+        // Use the locator to find the element again and return it
+        return driver.findElement(locator);
+    }
+
+    
+    private static By getLocatorFromElement(WebElement element) {
+        String[] data = element.toString().split(" -> ")[1].replaceFirst("(?s)(.*)\\]", "$1" ).split(": ");
+        String locatorType = data[0];
+        String locatorValue = data[1];
+
+        switch (locatorType) {
+            case "id":
+                return By.id(locatorValue);
+            case "name":
+                return By.name(locatorValue);
+            case "className":
+                return By.className(locatorValue);
+            case "tagName":
+                return By.tagName(locatorValue);
+            case "linkText":
+                return By.linkText(locatorValue);
+            case "partialLinkText":
+                return By.partialLinkText(locatorValue);
+            case "xpath":
+                return By.xpath(locatorValue);
+            case "cssSelector":
+                return By.cssSelector(locatorValue);
+            default:
+                throw new IllegalArgumentException("Cannot determine locator type: " + locatorType);
+        }
     }
 
 }
